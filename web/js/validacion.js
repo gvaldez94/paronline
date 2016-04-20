d18fb4270ebba60e877b4fdd229422537c2ffe96
Elias Maciel <eliasmacielr@gmail.com>
2016-04-20 19:08:22 -0400

    Documentos web. Fase de pruebas

---
+++ web/js/validacion.js
@@ -0,0 +1,73 @@
+var formLogin,
+    formRegistro;
+
+document.addEventListener("DOMContentLoaded", function() {
+    formLogin = document.forms['formLogin'];
+    formRegistro = document.forms['formRegistro'];
+});
+
+/* formLogin */
+function validarUserLogin() {
+    var usuario = formLogin['user'].value;
+    document.getElementById('userLoginMsg').innerHTML = (usuario == ''
+        ? 'Debe identificarse con un nombre de usuario.'
+        : '');
+}
+
+function validarPassLogin() {
+    var contrasenya = formLogin['pass'].value;
+    document.getElementById('passLoginMsg').innerHTML = (contrasenya == ''
+        ? 'Ingrese su contraseña.'
+        : '');
+}
+
+
+/* formRegistro */
+function validarNombre() {
+    var nombre = formRegistro['nombre'].value;
+    document.getElementById('nombreMsg').innerHTML = (nombre == ''
+        ? 'Debe ingresar su nombre.'
+        : '');
+}
+
+function validarApellido() {
+    var apellido = formRegistro['apellido'].value;
+    document.getElementById('apellidoMsg').innerHTML = (apellido == ''
+        ? 'Debe ingresar su apellido.'
+        : '');
+}
+
+function validarUser() {
+    var user = formRegistro['user'].value;
+    document.getElementById('userMsg').innerHTML = (user == ''
+        ? 'Debe ingresar un nombre de usuario.'
+        : '');
+}
+
+function validarEmail() {
+    var email = formRegistro['email'].value;
+    document.getElementById('emailMsg').innerHTML = (email == ''
+            ? 'Debe ingresar su dirección de correo.'
+            : '');
+}
+
+function validarPass() {
+    var pass_ver = formRegistro['pass_ver'].value;
+    if (pass_ver != '') {
+        var pass = formRegistro['pass'].value;
+        document.getElementById('pass_verMsg').innerHTML = (pass !== pass_ver
+            ? 'Las contraseñas no coinciden.'
+            : '');
+    } else {
+        document.getElementById('passMsg').innerHTML =
+            'Debe proporcionar una contraseña.';
+    }
+}
+
+function validarPass_ver() {
+    var pass = formRegistro['pass'].value,
+        pass_ver = formRegistro['pass_ver'].value;
+    document.getElementById('pass_verMsg').innerHTML = (pass !== pass_ver
+        ? 'Las contraseñas no coinciden.'
+        : '');
+}
