722675626fa122c04da928be81cc99a40a6fe2b0
gvaldez94 <francisco101991@gmail.com>
2016-04-23 16:53:11 +0200

    Agregado controlador para la busqueda de productos

---
+++ src/java/py/una/pol/par/controllers/Buscar.java
@@ -0,0 +1,109 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package py.una.pol.par.controllers;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import py.una.pol.par.dao.ProductoDao;
+import py.una.pol.par.models.Producto;
+
+/**
+ *
+ * @author Gabriel
+ */
+public class Buscar extends HttpServlet {
+
+    ProductoDao productoDao = new ProductoDao();
+    
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet Buscar</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet Buscar at " + request.getContextPath() + "</h1>");
+            
+            
+            try {
+                List<Producto> productos = productoDao.consultar(1, request.getParameter("producto"));
+                
+                if (!productos.isEmpty()) {
+                    for (Producto p : productos) {
+                        out.println("<h3>Descripcion: " + p.getDescripcion() + "</h3>");
+                        out.println("<p>Precio: " + p.getPrecioUnit() + "</p>");
+                    }
+                } else {
+                    out.println("<h2>No se encontraron productos</h2>");
+                }
+            out.println("</html>");
+            } catch (Exception e) {
+                out.println("<h2>Ocurrio un error: " + e.getMessage() + "</h2>");
+            }
+            
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
