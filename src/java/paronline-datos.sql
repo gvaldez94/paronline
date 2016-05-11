887100e8d2ef0542a1c26fc112b6abeec5b032f4
gvaldez94 <francisco101991@gmail.com>
2016-05-11 17:48:58 -0400

    Datos para las pruebas

--- src/java/paronline-datos.sql
+++ src/java/paronline-datos.sql
@@ -4,7 +4,7 @@
 
 -- Dumped from database version 9.4.4
 -- Dumped by pg_dump version 9.4.4
--- Started on 2016-05-09 22:31:23
+-- Started on 2016-05-11 17:46:29
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -26,6 +26,16 @@ INSERT INTO categoria (id, descripcion) VALUES (7, 'Utilitarios');
 
 
 --
+-- TOC entry 2084 (class 0 OID 0)
+-- Dependencies: 174
+-- Name: categoria_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('categoria_id_seq', 7, true);
+
+
+
+--
 -- TOC entry 2066 (class 0 OID 24733)
 -- Dependencies: 183
 -- Data for Name: unidad; Type: TABLE DATA; Schema: public; Owner: postgres
@@ -43,15 +53,6 @@ INSERT INTO unidad (id, descripcion) VALUES (3, 'decenas');
 
 SELECT pg_catalog.setval('unidad_id_seq', 3, true);
 
-
-
---
--- TOC entry 2084 (class 0 OID 0)
--- Dependencies: 174
--- Name: categoria_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
---
-
-SELECT pg_catalog.setval('categoria_id_seq', 7, true);
 
 
 --
@@ -80,10 +81,9 @@ SELECT pg_catalog.setval('producto_id_seq', 4, true);
 -- Data for Name: stock; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-INSERT INTO stock (id, producto_id, cantidad) VALUES (2, 4, 200);
-INSERT INTO stock (id, producto_id, cantidad) VALUES (5, 4, 10);
-INSERT INTO stock (id, producto_id, cantidad) VALUES (3, 3, 46);
-INSERT INTO stock (id, producto_id, cantidad) VALUES (4, 1, 198);
+INSERT INTO stock (id, producto_id, cantidad) VALUES (3, 3, 42);
+INSERT INTO stock (id, producto_id, cantidad) VALUES (4, 1, 196);
+INSERT INTO stock (id, producto_id, cantidad) VALUES (2, 4, 192);
 
 
 --
@@ -92,7 +92,7 @@ INSERT INTO stock (id, producto_id, cantidad) VALUES (4, 1, 198);
 -- Name: stock_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('stock_id_seq', 5, true);
+SELECT pg_catalog.setval('stock_id_seq', 6, true);
 
 
 --
@@ -102,6 +102,13 @@ SELECT pg_catalog.setval('stock_id_seq', 5, true);
 --
 
 INSERT INTO transacciones_cab (id, fecha, total, direccion, medio_pago, nro_tarjeta, estado, usuario_id) VALUES (16, '2016-05-09', 360000.00, 'asdfadf', 1, '1321354654', 'I', 2);
+INSERT INTO transacciones_cab (id, fecha, total, direccion, medio_pago, nro_tarjeta, estado, usuario_id) VALUES (17, '2016-05-10', 300000.00, 'asdf', 1, '465464978', 'I', 2);
+INSERT INTO transacciones_cab (id, fecha, total, direccion, medio_pago, nro_tarjeta, estado, usuario_id) VALUES (18, '2016-05-11', 666000.00, 'asdf', 1, '5464679797', 'I', 2);
+INSERT INTO transacciones_cab (id, fecha, total, direccion, medio_pago, nro_tarjeta, estado, usuario_id) VALUES (19, '2016-05-11', 60000.00, 'asdf', 0, NULL, 'I', 2);
+INSERT INTO transacciones_cab (id, fecha, total, direccion, medio_pago, nro_tarjeta, estado, usuario_id) VALUES (20, '2016-05-11', 300000.00, 'asdf', 0, NULL, 'I', 2);
+INSERT INTO transacciones_cab (id, fecha, total, direccion, medio_pago, nro_tarjeta, estado, usuario_id) VALUES (21, '2016-05-11', 300000.00, 'asdf', 0, NULL, 'I', 2);
+INSERT INTO transacciones_cab (id, fecha, total, direccion, medio_pago, nro_tarjeta, estado, usuario_id) VALUES (22, '2016-05-11', 366000.00, 'asdf', 1, '654649787', 'I', 2);
+INSERT INTO transacciones_cab (id, fecha, total, direccion, medio_pago, nro_tarjeta, estado, usuario_id) VALUES (23, '2016-05-11', 600000.00, 'adsf', 1, '65464897', 'I', 2);
 
 
 --
@@ -110,7 +117,7 @@ INSERT INTO transacciones_cab (id, fecha, total, direccion, medio_pago, nro_tarj
 -- Name: transacciones_cab_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('transacciones_cab_id_seq', 16, true);
+SELECT pg_catalog.setval('transacciones_cab_id_seq', 23, true);
 
 
 --
@@ -121,6 +128,17 @@ SELECT pg_catalog.setval('transacciones_cab_id_seq', 16, true);
 
 INSERT INTO transacciones_det (id, transacciones_cab_id, item, cantidad, precio, subtotal, producto_id) VALUES (1, 16, 1, 2, 150000.00, 300000.00, 4);
 INSERT INTO transacciones_det (id, transacciones_cab_id, item, cantidad, precio, subtotal, producto_id) VALUES (2, 16, 2, 2, 30000.00, 60000.00, 1);
+INSERT INTO transacciones_det (id, transacciones_cab_id, item, cantidad, precio, subtotal, producto_id) VALUES (3, 17, 1, 2, 150000.00, 300000.00, 4);
+INSERT INTO transacciones_det (id, transacciones_cab_id, item, cantidad, precio, subtotal, producto_id) VALUES (4, 18, 1, 4, 150000.00, 600000.00, 4);
+INSERT INTO transacciones_det (id, transacciones_cab_id, item, cantidad, precio, subtotal, producto_id) VALUES (5, 18, 2, 2, 3000.00, 6000.00, 3);
+INSERT INTO transacciones_det (id, transacciones_cab_id, item, cantidad, precio, subtotal, producto_id) VALUES (6, 18, 3, 2, 30000.00, 60000.00, 1);
+INSERT INTO transacciones_det (id, transacciones_cab_id, item, cantidad, precio, subtotal, producto_id) VALUES (7, 19, 1, 2, 30000.00, 60000.00, 1);
+INSERT INTO transacciones_det (id, transacciones_cab_id, item, cantidad, precio, subtotal, producto_id) VALUES (8, 20, 1, 2, 150000.00, 300000.00, 4);
+INSERT INTO transacciones_det (id, transacciones_cab_id, item, cantidad, precio, subtotal, producto_id) VALUES (9, 21, 1, 2, 150000.00, 300000.00, 4);
+INSERT INTO transacciones_det (id, transacciones_cab_id, item, cantidad, precio, subtotal, producto_id) VALUES (10, 22, 1, 2, 150000.00, 300000.00, 4);
+INSERT INTO transacciones_det (id, transacciones_cab_id, item, cantidad, precio, subtotal, producto_id) VALUES (11, 22, 2, 2, 3000.00, 6000.00, 3);
+INSERT INTO transacciones_det (id, transacciones_cab_id, item, cantidad, precio, subtotal, producto_id) VALUES (12, 22, 3, 2, 30000.00, 60000.00, 1);
+INSERT INTO transacciones_det (id, transacciones_cab_id, item, cantidad, precio, subtotal, producto_id) VALUES (13, 23, 1, 4, 150000.00, 600000.00, 4);
 
 
 --
@@ -129,7 +147,7 @@ INSERT INTO transacciones_det (id, transacciones_cab_id, item, cantidad, precio,
 -- Name: transacciones_det_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('transacciones_det_id_seq', 2, true);
+SELECT pg_catalog.setval('transacciones_det_id_seq', 13, true);
 
 
 --
@@ -138,10 +156,9 @@ SELECT pg_catalog.setval('transacciones_det_id_seq', 2, true);
 -- Data for Name: usuarios; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-INSERT INTO usuarios (id, nombre, apellido, login_name, passwd, tipo_usuario) VALUES (1, 'admin', 'admin', 'admin', '21232f297a57a5a743894a0e4a801fc3', 0);
-INSERT INTO usuarios (id, nombre, apellido, login_name, passwd, tipo_usuario) VALUES (2, 'Gabriel', 'Valdez', 'gvaldez', 'd0a4a9d5eae1444b3285be84e98afcf8', 1);
-INSERT INTO usuarios (id, nombre, apellido, login_name, passwd, tipo_usuario) VALUES (7, 'Elias', 'Maciel', 'emaciel', '4d186321c1a7f0f354b297e8914ab240', 1);
-INSERT INTO usuarios (id, nombre, apellido, login_name, passwd, tipo_usuario) VALUES (8, 'Monika', 'Nagy', 'mnagy', '912ec803b2ce49e4a541068d495ab570', 1);
+INSERT INTO usuarios (id, nombre, apellido, login_name, passwd, tipo_usuario, email) VALUES (1, 'admin', 'admin', 'admin', '21232f297a57a5a743894a0e4a801fc3', 0, 'example@example.com');
+INSERT INTO usuarios (id, nombre, apellido, login_name, passwd, tipo_usuario, email) VALUES (2, 'Gabriel', 'Valdez', 'gvaldez', 'd0a4a9d5eae1444b3285be84e98afcf8', 1, 'example@example.com');
+INSERT INTO usuarios (id, nombre, apellido, login_name, passwd, tipo_usuario, email) VALUES (7, 'Elias', 'Maciel', 'emaciel', '4d186321c1a7f0f354b297e8914ab240', 1, 'example@example.com');
 
 
 --
@@ -150,13 +167,12 @@ INSERT INTO usuarios (id, nombre, apellido, login_name, passwd, tipo_usuario) VA
 -- Name: usuarios_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('usuarios_id_seq', 8, true);
+SELECT pg_catalog.setval('usuarios_id_seq', 9, true);
 
 
--- Completed on 2016-05-09 22:31:24
+-- Completed on 2016-05-11 17:46:30
 
 --
 -- PostgreSQL database dump complete
 --
 
- 
